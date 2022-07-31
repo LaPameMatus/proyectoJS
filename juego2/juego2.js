ea5aaf0a1fa97745876a7e6f4081a6eb7529585f
diego <contact@fartweb.com>
2022-07-31 15:36:46 -0300

    subiendo primer formato juego2

---
+++ juego2/juego2.js
@@ -0,0 +1,32 @@
+correctas = [3, 1, 2, 2, 3];
+
+//arreglo donde se guardan las repuestas del usuario}
+opcion_elegida = [];
+cantidad_correctas = 0;
+//funcion que toma el num de preg y el imput elegido de esa pregunta
+function repuesta(num_pregunta, seleccionada) {
+  //guardo la rep elegida
+  opcion_elegida[num_pregunta] = seleccionada.value;
+  // el siguiente codigo es para poner el color en blanco
+  //el fondo de los inputs para cuando elige otra ocpcion
+  //armo el id para seleccionar el section correspondiente
+  id = "p" + num_pregunta;
+  labels = document.getElementById(id).childNodes;
+  labels[3].style.backgroundColor = "white";
+  labels[5].style.backgroundColor = "white";
+  labels[7].style.backgroundColor = "white";
+  //doy el color a label seleccionado
+  seleccionada.parentNode.style.backgroundColor = "#cec0fc";
+}
+
+// funcion que compara los arreglos para saber cuantas estuvieran correctas
+function corregir() {
+  cantidad_correctas = 0;
+  for (i = 0; i < correctas.length; i++) {
+    if (correctas[i] == opcion_elegida[i]) {
+      cantidad_correctas++;
+    }
+  }
+
+  document.getElementById("resultado").innerHTML = cantidad_correctas;
+}
