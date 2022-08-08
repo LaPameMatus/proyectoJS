a6282a3df081661e62c357aed1458c6cf9c63fe0
diego <contact@fartweb.com>
2022-08-07 23:49:34 -0300

    pagina contacto con efecto

--- juego2/contacto.js
+++ juego2/contacto.js
@@ -11,3 +11,25 @@ function enviar() {
 
   alert(cartel);
 }
+
+// fondo
+const animator = document.querySelector(".animator");
+const colors = ["#04FF00 ", "#F0FF00 ", "#0018B2 ", "#FF01F3", "#FF0101 "];
+
+const figures = () => {
+  for (let i = 0; i <= 25; i++) {
+    let figure = document.createElement("span");
+    let select = Math.round(colors.length * Math.random());
+
+    figure.style.top = innerHeight * Math.random() + "px";
+    figure.style.left = innerWidth * Math.random() + "px";
+    figure.style.background =
+      colors[select >= colors.length ? colors - 1 : select];
+    animator.appendChild(figure);
+    setInterval(() => {
+      figure.style.top = innerHeight * Math.random() + "px";
+      figure.style.left = innerWidth * Math.random() + "px";
+    }, 5000);
+  }
+};
+figures();
